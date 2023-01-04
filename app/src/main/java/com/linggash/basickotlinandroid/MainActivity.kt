313c54067898f8232d110d6f74185f27ac16d1a8
linggash <sempanalingga@gmail.com>
2023-01-04 13:56:08 +0700

    lateinit practice

--- app/src/main/java/com/linggash/basickotlinandroid/MainActivity.kt
+++ app/src/main/java/com/linggash/basickotlinandroid/MainActivity.kt
@@ -7,13 +7,23 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var nameEditText: EditText
+    private lateinit var sayHelloButton: Button
+    private lateinit var sayHelloTextView: TextView
+
+    private fun initComponent(){
+        nameEditText =findViewById(R.id.nameEditText)
+        sayHelloButton = findViewById(R.id.sayHelloButton)
+        sayHelloTextView = findViewById(R.id.sayHelloTextView)
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hello_world)
 
-        val nameEditText: EditText = findViewById(R.id.nameEditText)
-        val sayHelloButton: Button = findViewById(R.id.sayHelloButton)
-        val sayHelloTextView : TextView = findViewById(R.id.sayHelloTextView)
+        initComponent()
 
         sayHelloTextView.text = "hi"
 
