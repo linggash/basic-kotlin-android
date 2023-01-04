32f7c3110ab348e026c79dabc4d05dab909ac991
linggash <sempanalingga@gmail.com>
2023-01-04 13:38:21 +0700

    view id practice

--- app/src/main/java/com/linggash/basickotlinandroid/MainActivity.kt
+++ app/src/main/java/com/linggash/basickotlinandroid/MainActivity.kt
@@ -2,10 +2,19 @@ package com.linggash.basickotlinandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hello_world)
+
+        val nameEditText: EditText = findViewById(R.id.nameEditText)
+        val sayHelloButton: Button = findViewById(R.id.sayHelloButton)
+        val sayHelloTextView : TextView = findViewById(R.id.sayHelloTextView)
+
+        sayHelloTextView.text = "hi"
     }
 }
