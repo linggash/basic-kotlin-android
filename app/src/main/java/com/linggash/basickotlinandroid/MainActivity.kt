2f46e2e2bdb917b40aeb1586c0ddfc74f3a61c74
linggash <sempanalingga@gmail.com>
2023-01-04 14:07:28 +0700

    log practice

--- app/src/main/java/com/linggash/basickotlinandroid/MainActivity.kt
+++ app/src/main/java/com/linggash/basickotlinandroid/MainActivity.kt
@@ -2,6 +2,7 @@ package com.linggash.basickotlinandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -28,6 +29,12 @@ class MainActivity : AppCompatActivity() {
         sayHelloTextView.text = "hi"
 
         sayHelloButton.setOnClickListener {
+
+            Log.d("LSH", "This is debug log")
+            Log.i("LSH", "This is info log")
+            Log.w("LSH", "This is warn log")
+            Log.e("LSH", "This is error log")
+
             val name = nameEditText.text.toString()
             sayHelloTextView.text = "Hi $name"
         }
