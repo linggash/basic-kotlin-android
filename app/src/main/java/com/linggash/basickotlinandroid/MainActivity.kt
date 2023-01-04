badd46e012b0d25b7cef31a68e26f8f2b70aee16
linggash <sempanalingga@gmail.com>
2023-01-04 20:58:02 +0700

    asset manager practice

--- app/src/main/java/com/linggash/basickotlinandroid/MainActivity.kt
+++ app/src/main/java/com/linggash/basickotlinandroid/MainActivity.kt
@@ -33,6 +33,12 @@ class MainActivity : AppCompatActivity() {
         // action listener practice
         sayHelloButton.setOnClickListener {
 
+            val json = assets.open("sample.json")
+                .bufferedReader()
+                .use { it.readText() }
+
+            Log.i("ASSET", json)
+
 //            resources.getDrawable(R.drawable.mamike, theme)
 
             //log practice
