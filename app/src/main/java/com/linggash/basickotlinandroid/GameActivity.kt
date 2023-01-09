750807bba9b6f4d6ef455f28b420d5e63d405dc0
linggash <sempanalingga@gmail.com>
2023-01-09 21:02:45 +0700

    game layout created

---
+++ app/src/main/java/com/linggash/basickotlinandroid/GameActivity.kt
@@ -0,0 +1,12 @@
+package com.linggash.basickotlinandroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.game)
+    }
+}
